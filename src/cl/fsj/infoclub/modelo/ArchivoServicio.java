e42df1c603d8a1ebb4eeaf23bea86656756f8b7e
QukkiQ <oscar.zapata.g@mail.pucv.cl>
2022-07-29 21:22:30 -0400

    Avance ArchivoServicio.java/cargarDatos()

--- src/cl/fsj/infoclub/modelo/ArchivoServicio.java
+++ src/cl/fsj/infoclub/modelo/ArchivoServicio.java
@@ -1,5 +1,34 @@
 package cl.fsj.infoclub.modelo;
 
-public class ArchivoServicio {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
+public class ArchivoServicio {
+	
+	public void cargarDatos() {
+		try {
+			File archivo = new File("src/ProductosImportados.csv");
+			if (archivo.exists()) {
+				FileReader fr = new FileReader(archivo);
+				BufferedReader br = new BufferedReader(fr);
+				String data = br.readLine();
+							
+				while (data != null) {
+					String[] variables = data.split("\\,");
+					System.out.println(variables[3]);
+					System.out.println(data);  	 
+					data = br.readLine();
+				}
+				
+
+				br.close(); 
+			} else {
+				System.out.println("El fichero ingresado no existe");
+			}
+
+		} catch (Exception e) {
+			System.out.println("Excepcion leyendo fichero : " + e);
+		}
+	}
 }
