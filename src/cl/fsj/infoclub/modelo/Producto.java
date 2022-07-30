c4610e15b572ea7901397ca22f9b6bb7a14160a5
Paulette Pleticosic <p.pleticosic@gmail.com>
2022-07-29 20:18:04 -0400

    Clase producto lista

--- src/cl/fsj/infoclub/modelo/Producto.java
+++ src/cl/fsj/infoclub/modelo/Producto.java
@@ -2,4 +2,77 @@ package cl.fsj.infoclub.modelo;
 
 public class Producto {
 	String articulo, precio, descripcion, codigo, talla, marca, color;
+	
+	public Producto() {
+		super();
+	}
+
+	public Producto(String articulo, String precio, String descripcion, String codigo, String talla, String marca,
+			String color) {
+		super();
+		this.articulo = articulo;
+		this.precio = precio;
+		this.descripcion = descripcion;
+		this.codigo = codigo;
+		this.talla = talla;
+		this.marca = marca;
+		this.color = color;
+	}
+
+	public String getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(String articulo) {
+		this.articulo = articulo;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTalla() {
+		return talla;
+	}
+
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
 }
